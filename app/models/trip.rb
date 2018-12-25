97e429abf90a63db33166767cb42f1879bcce9ca
enciie <enciie@gmail.com>
2018-12-24 21:40:50 -0800

    Create model validations and relationships

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,6 @@
 class Trip < ApplicationRecord
+    belongs_to :user
+    has_many :comments
+
+    validates :name, :description, :locataion, :img_url, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :name, :hometown, :password, presence: true
+    validates :username, presence: true
+    validates :email, presence: true
+
+    has_many :trips
 end
