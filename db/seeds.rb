ed8a8077b8a6182f15d7666937abae0d6b7b27d7
enciie <enciie@gmail.com>
2018-12-27 15:16:14 -0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(name:"George Clooney", username:"GroovyChedda", email:"groovychedda@mail.com", password:"password", password_confirmation:"password", hometown:"LA" )
+
+trip = user.trips.build(name: "Winter Wonderland", description:"January 2019 first trip of the year", location:"Switzlerand", img_url:"https://i.imgur.com/jPlp1o4.jpg")
+trip.save
+
+trip.comments.build(content: "Can't wait!").save
+
+user_b = User.create(name:"Brad Pitt", username:"BravePerson", email:"braveperson@mail.com", password:"password", password_confirmation:"password", hometown:"LA")
+
+trip_b = user_b.trips.build(name: "Kiwi Adventure", description:"Backpacking trip to Sotuh New Zealand", location:"Queenstown, NZ", img_url:"https://i.imgur.com/SjNlE7v.jpg" )
+trip_b.save
+
+trip_b.comments.build(content: "Breathtaking views and endless adventures").save
