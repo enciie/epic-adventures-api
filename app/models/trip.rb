2737a82b0065db74e007bc013b13477263bd4906
enciie <enciie@gmail.com>
2018-12-27 15:58:38 -0800

    Add model associations and validations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,5 +2,5 @@ class Trip < ApplicationRecord
     belongs_to :user
     has_many :comments
 
-    validates :name, :description, :locataion, :img_url, presence: true
+    validates :name, :description, :location, :img_url, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :name, :hometown, :password, presence: true
-    validates :username, presence: true
-    validates :email, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 
     has_many :trips
 end
