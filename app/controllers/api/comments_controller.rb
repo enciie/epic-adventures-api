ef70e38c5276b2f9c976ec5ec90d70e5211f7cf6
enciie <enciie@gmail.com>
2018-12-27 16:00:43 -0800

    Create comments controller actions

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,33 @@
+class Api::CommentsController < ApplicationController
+    def index
+        @trip = Trip.find_by(id: params[:trip_id])
+
+        render json: @trip.comments, status: 200
+    end
+
+    def create
+        @trip = Trip.find_by(id: params[:id])
+        @comment = @trip.comments.build(comment_params)
+        if @comment.save
+            render json: @comment, status: 201
+        else
+            render json: { 
+                errors: { 
+                    messages: @comment.errors.messages 
+                } 
+            }, status: 422
+        end
+    end
+
+    def destroy
+        @trip = Trip.find_by(id: params[:id])
+        @comment = @trip.comments.find_by(id: params[:id])
+        @comment.destroy
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
+end
