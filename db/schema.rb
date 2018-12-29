42b87e29b8c1886468188d444e90b43e59d4b606
enciie <enciie@gmail.com>
2018-12-28 22:44:24 -0800

    Remove name and hometown from user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_27_213341) do
+ActiveRecord::Schema.define(version: 2018_12_29_004750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,11 +37,9 @@ ActiveRecord::Schema.define(version: 2018_12_27_213341) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "username"
     t.string "email"
     t.string "password_digest"
-    t.string "hometown"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create(name:"George Clooney", username:"GroovyChedda", email:"groovychedda@mail.com", password:"password", password_confirmation:"password", hometown:"LA" )
+user = User.create(username:"GroovyChedda", email:"groovychedda@mail.com", password:"password", password_confirmation:"password")
 
 trip = user.trips.build(name: "Winter Wonderland", description:"January 2019 first trip of the year", location:"Switzlerand", img_url:"https://i.imgur.com/jPlp1o4.jpg")
 trip.save
 
 trip.comments.build(content: "Can't wait!", user_id: "4").save
 
-user_b = User.create(name:"Brad Pitt", username:"BravePerson", email:"braveperson@mail.com", password:"password", password_confirmation:"password", hometown:"LA")
+user_b = User.create(username:"BravePerson", email:"braveperson@mail.com", password:"password", password_confirmation:"password")
 
 trip_b = user_b.trips.build(name: "Kiwi Adventure", description:"Backpacking trip to Sotuh New Zealand", location:"Queenstown, NZ", img_url:"https://i.imgur.com/SjNlE7v.jpg" )
 trip_b.save
