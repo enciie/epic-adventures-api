9b1bc27940961183d090018225371aab90e7fa18
enciie <enciie@gmail.com>
2018-12-28 22:46:40 -0800

    Create Trip serializer

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,5 +1,6 @@
 class TripSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :location, :img_url, :user_id
+  attributes :id, :name, :description, :location, :img_url
 
   has_many :comments
+  belongs_to :user
 end
