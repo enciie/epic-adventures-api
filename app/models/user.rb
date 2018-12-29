f02672d646ff7c86062d7da6e514988d02e49fea
enciie <enciie@gmail.com>
2018-12-28 22:47:57 -0800

    Remove name and hometown validations from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :name, :hometown, :password, presence: true
+    validates :password, presence: true
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
 
