39952e40fba839ba11b9f0f3942671c5f9f4ae31
enciie <enciie@gmail.com>
2018-12-28 22:49:58 -0800

    Fix argument for update action

--- app/controllers/api/trips_controller.rb
+++ app/controllers/api/trips_controller.rb
@@ -21,7 +21,7 @@ class Api::TripsController < ApplicationController
 
     def update
         @trip = Trip.find_by(id: params[:id])
-        if @trip.update(surfboard_params)
+        if @trip.update(trip_params)
             render json: @trip
         else
             render json: { message: @trip.errors }, status: 400
